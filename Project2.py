f8741d0e4859bb989518edcd7f7395b1ab0edfa2
shreyas-reddy18 <snallapa1@gmail.com>
2025-11-23 15:56:52 -0800

    Introduced first function which returns random percentage for accuracy

---
+++ Project2.py
@@ -0,0 +1,20 @@
+"""
+CS170 Project 2: Part 1
+
+Authors: Shreyas, Rishabh, Rehan
+"""
+
+
+def leave_one_out_cross_validation(feature_subset, data=None):
+    """
+    Returns random accuracy.
+    In Part II, this will be replaced with actual k-NN classifier.
+    
+    Args:
+        feature_subset: List of feature indices to evaluate
+        data: Dataset (unused in Part I stub)
+    
+    Returns:
+        Random accuracy percentage (float)
+    """
+    return round(random.uniform(0, 100), 1)
